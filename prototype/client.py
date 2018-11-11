c180d1c6547d03faf8a972c8c53042bcf1b962d5
Johan Stenberg <johan.stenberg@microsoft.com>
2018-11-10 17:07:00 -0800

    Make Database and string interchangeable

--- prototype/client.py
+++ prototype/client.py
@@ -1,11 +1,15 @@
-from typing import TYPE_CHECKING, cast
-
-if TYPE_CHECKING:
-    from typing import Any, Iterable, Optional, Dict, Union, Tuple
+"""
+Cosmos client Pythonidae
+"""
+from typing import Any, Iterable, Optional, Dict, Union, Tuple, cast
 
 
 from azure.cosmos.cosmos_client import CosmosClient as _CosmosClient
 from azure.cosmos.errors import HTTPFailure
+
+
+class ClientSession(_CosmosClient):
+    pass
 
 
 class UsersManagementMixin:
@@ -77,22 +81,29 @@ class ContainersManagementMixin:
     """
     Manage (create/list/query/get/delete) containers. 
     [Design note] Currently isolated into a mixin class to make it easier to move the functionality around
-                  in the object model. 
+    in the object model. 
     """
 
-    def create_container(self, id, options=None, **kwargs) -> "Container":
+    def create_container(
+        self, id, options=None, **kwargs
+    ) -> "Container":
         """
         :param str id: Id of container to create
+
         Keyword arguments:
         partitionKey, indexingPolicy, defaultTtl, conflictResolutionPolicy
         """
         definition = dict(id=id)
         definition.update(kwargs)
         database = cast("Database", self)
-        data = database._context.CreateContainer(
-            database_link=database.database_link, collection=definition, options=options
+        data = database.client_session.CreateContainer(
+            database_link=database.database_link,
+            collection=definition,
+            options=options,
         )
-        return Container(database, data["id"])
+        return Container(
+            cast("Database", self).client_session, database, data["id"]
+        )
 
     def delete_container(self, container_or_id):
         database = cast("Database", self)
@@ -119,28 +130,60 @@ class ContainersManagementMixin:
 
         if query:
             yield from [
-                Container(database, container["id"])
-                for container in database._context.ReadContainers(
+                Container(database.client_session, database, container["id"])
+                for container in database.client_session.ReadContainers(
                     database_link=database.database_link
                 )
             ]
         else:
             yield from [
-                Container(database, container["id"])
-                for container in database._context.ReadContainers(
+                Container(database.client_session, database, container["id"])
+                for container in database.client_session.ReadContainers(
                     database_link=database.database_link
                 )
             ]
 
+    def set_container_properties(
+        self,
+        container: "Union[str, Container]",
+        *,
+        partition_key=None,
+        indexing_policy=None,
+        default_ttl=None,
+        conflict_resolution_policy=None,
+    ):
+        """
+        Update the properties of the container. Change will be persisted immediately.
+        """
+        id = getattr(container, "id", container)
+        parameters = {
+            key: value
+            for key, value in {
+                "id": id,
+                "partitionKey": partition_key,
+                "indexingPolicy": indexing_policy,
+                "defaultTtl": int(default_ttl),
+                "conflictResolutionPolicy": conflict_resolution_policy,
+            }.items()
+            if value
+            is not None  # TODO: Questionable use - should use kwargs instead. Need to figure out best documentation for kwargs...
+        }
+        database = cast("Database", self)
+        collection_link = f"{database.database_link}/colls/{id}"
+        database.client_session.ReplaceContainer(collection_link, collection=parameters)
 
-class Client:
-    """
-    CosmosDB SQL Client. This is the main entry point to the Cosmos DB object model.
-    """
+    def get_container_properties(self, container):
+        """
+        Get properties for of container. TODO: implement properly
+        """
+        pass
 
-    def __init__(self, url, key):
-        self._context = _CosmosClient(url, dict(masterKey=key))
 
+class ItemsManagementMixin:
+    pass
+
+
+class DatabasesManagementMixin:
     def create_database(self, id: "str", fail_if_exists=False) -> "Database":
         """
         Create a new database
@@ -150,41 +193,44 @@ class Client:
         the given id exists, and `fail_if_exists` is False, return the existing database.
         """
         try:
-            result = self._context.CreateDatabase(database=dict(id=id))
-            return Database(self, id=result["id"])
+            client_session = cast("Client", self).client_session
+            result = client_session.CreateDatabase(database=dict(id=id))
+            return Database(client_session, id=result["id"], properties=result)
         except HTTPFailure as e:
             if fail_if_exists and e.status_code == 409:
                 raise
         return self.get_database(id)
 
-    def get_database(self, id: "str", metadata_handler=None) -> "Database":
+    def get_database(self, id: "str") -> "Database":
         """
         Return the existing database with the id `id. 
         :param str id: Id of the new database.
-        :param callable metadata_handler: Optional method that will, if provided, receive an instance
-        of `CosmosCallMetadata` representing metadata about the operation (RSU cost etc.)
         """
-        database = Database(client=self, id=id)
-        if metadata_handler is not None:
-            self._context.ReadDatabase(database.database_link)
-            # TODO: Parse last response headers into strongly typed object
-            metadata_handler(self._context.last_response_headers)
+        client_session = cast("Client", self).client_session
+        properties = client_session.ReadDatabase(database_link=f"dbs/{id}")
+        database = Database(client_session, id, properties)
         return database
+
+    def get_database_properties(self, id: "str"):
+        client_session = cast("Client", self).client_session
+        properties = client_session.ReadDatabase(id)
+        return properties
 
     def list_databases(self, query: "Optional[str]" = None) -> "Iterable[Database]":
         """
         Return an iterable of all existing databases.
         :param str query: Cosmos DB SQL query. If omitted, all databases will be listed.
         """
+        client_session = cast("Client", self).client_session
         if query:
             yield from [
-                Database(self, database["id"])
-                for database in self._context.QueryDatabases(query)
+                Database(client_session, properties["id"], properties)
+                for properties in client_session.QueryDatabases(query)
             ]
         else:
             yield from [
-                Database(self, database["id"])
-                for database in self._context.ReadDatabases()
+                Database(client_session, properties["id"], properties)
+                for properties in client_session.ReadDatabases()
             ]
 
     def delete_database(self, id: "str"):
@@ -193,7 +239,23 @@ class Client:
         the delete fails. 
         :param str id: The database to delete. 
         """
-        self._context.DeleteDatabase(database_link="dbs/" + id)
+        client_session = cast("Client", self).client_session
+        client_session.DeleteDatabase(database_link=f"dbs/{id}")
+
+
+class Client(DatabasesManagementMixin):
+    """
+    CosmosDB SQL Client. This is the main entry point to the Cosmos DB object model.
+
+    >>> import os
+    >>> ACCOUNT_KEY = os.environ['ACCOUNT_KEY']
+    >>> ACCOUNT_HOST = os.environ['ACCOUNT_HOST']
+    >>> client = Client(url=ACCOUNT_HOST, key=ACCOUNT_KEY)
+    ...
+    """
+
+    def __init__(self, url, key):
+        self.client_session = ClientSession(url, dict(masterKey=key))
 
 
 class Database(ContainersManagementMixin, UsersManagementMixin):
@@ -201,68 +263,38 @@ class Database(ContainersManagementMixin, UsersManagementMixin):
     Azure Cosmos DB SQL Database
     """
 
-    def __init__(self, client: "Client", id: "str"):
+    def __init__(
+        self, client_session: "ClientSession", id: "str", properties: "Dict[str, Any]"
+    ):
         """
-        :param Client client: Client from which this database was retreived. TODO: should we hide the client? Should it just be context?
+        :param ClientSession client_session: Client from which this database was retreived.
         :param str id: Id of the database
         """
-        self.client = client
-        self._context = client._context
+        self.client_session = client_session
         self.id = id
+        self.properties = properties
         self.database_link = f"/dbs/{self.id}"
 
 
 class Item(dict):
-    def __init__(self, container: "Container", headers: "Dict[str, Any]", data: "Dict[str, Any]"):
+    def __init__(self, headers: "Dict[str, Any]", data: "Dict[str, Any]"):
         super().__init__()
-        self.container = (
-            container
-        )  # TODO: Item instances (locally) probably shouldn't be directly tied to a collection
-        self._context = container._context
         self.response_headers = headers
         self.update(data)
 
 
 class Container(UserDefinedFunctionsMixin, TriggersMixin, StoredProceduresMixin):
-    def __init__(self, database: "Database", id: "str"):
-        self.database = database
-        self._context = database._context
+    def __init__(
+        self,
+        client_session: "ClientSession",
+        database: "Union[Database, str]",
+        id: "str",
+    ):
+        self.client_session = client_session
         self.session_token = None
         self.id = id
-        self.collection_link = f"{database.database_link}/colls/{self.id}"
-
-    def set_properties(
-        self,
-        *,
-        id=None,
-        partition_key=None,
-        indexing_policy=None,
-        default_ttl=None,
-        conflict_resolution_policy=None,
-    ):
-        """
-        Update the properties of the container. Change will be persisted immediately.
-        TODO: Should this be on the database class?
-        """
-        parameters = {
-            key: value
-            for key, value in {
-                "id": id or self.id,
-                "partitionKey": partition_key,
-                "indexingPolicy": indexing_policy,
-                "defaultTtl": int(default_ttl),
-                "conflictResolutionPolicy": conflict_resolution_policy,
-            }.items()
-            if value
-            != None  # TODO: Questionable use - should use kwargs instead. Need to figure out best documentation for kwargs...
-        }
-        self._context.ReplaceContainer(self.collection_link, collection=parameters)
-
-    def get_properties(self, force_refresh=False):
-        """
-        Get properties for of container. TODO: implement properly
-        """
-        pass
+        database_link = getattr(database, "database_link", f'dbs/{database}')
+        self.collection_link = f"{database_link}/colls/{self.id}"
 
     @staticmethod
     def _document_link(item_or_link) -> "str":
@@ -270,55 +302,60 @@ class Container(UserDefinedFunctionsMixin, TriggersMixin, StoredProceduresMixin)
             return item_or_link
         return cast("str", cast("Item", item_or_link)["_self"])
 
-    def get_item(self, id: "str", metadata_handler=None, cls=Item) -> "Item":
+    def get_item(self, id: "str") -> "Item":
         """
         Get the item identified by `id`
         :param str id: Id of item to retreive
         :param callable metadata_handler: Optional method that will, if provided, receive an instance
         of `CosmosCallMetadata` representing metadata about the operation (RSU cost etc.)
-        :returns: Item if present. TODO: General guidelines is to throw rather than returning None. 
+        :returns: Item if present.
         """
         doc_link = f"{self.collection_link}/docs/{id}"
-        result = self._context.ReadItem(document_link=doc_link)
-        headers = self._context.last_response_headers
-        if metadata_handler:  # TODO: Create strongly typed object for options
-            metadata_handler(headers)
+        result = self.client_session.ReadItem(document_link=doc_link)
+        headers = self.client_session.last_response_headers
         self.session_token = headers.get("x-ms-session-token", self.session_token)
-        return cls(container=self, headers=self._context.last_response_headers, data=result)
+        return Item(headers=headers, data=result)
 
-    def list_items(self, options=None, cls=Item) -> "Iterable[Item]":
+    def list_items(self, options=None) -> "Iterable[Item]":
         options = options or {}
-        items = self._context.ReadItems(
+        items = self.client_session.ReadItems(
             collection_link=self.collection_link, feed_options=options
         )
-        headers = self._context.last_response_headers
-        yield from [cls(self, headers=headers, data=item) for item in items]
+        headers = self.client_session.last_response_headers
+        yield from [Item(headers=headers, data=item) for item in items]
 
-    def query_items(self, query: "str", cls=Item):
-        items = self._context.QueryItems(
-            database_or_Container_link=self.collection_link, query=query
+    def query_items(
+        self, query: "str", options=None, partition_key: "Optional[str]" = None
+    ):
+        items = self.client_session.QueryItems(
+            database_or_Container_link=self.collection_link,
+            query=query,
+            options=options,
+            partition_key=partition_key,
         )
-        headers = self._context.last_response_headers
-        yield from [cls(self, headers, item) for item in items]
+        headers = self.client_session.last_response_headers
+        yield from [Item(headers, item) for item in items]
 
     def replace_item(self, item: "Union[Item, str]", body: "Dict[str, Any]") -> "Item":
         item_link = Container._document_link(item)
-        data = self._context.ReplaceItem(document_link=item_link, new_document=body)
-        return Item(self, headers=self._context.last_response_headers, data=data)
+        data = self.client_session.ReplaceItem(
+            document_link=item_link, new_document=body
+        )
+        return Item(headers=self.client_session.last_response_headers, data=data)
 
     def upsert_item(self, body: "Dict[str, Any]") -> "Item":
-        result = self._context.UpsertItem(
+        result = self.client_session.UpsertItem(
             database_or_Container_link=self.collection_link, document=body
         )
-        return Item(self, headers=self._context.last_response_headers, data=result)
+        return Item(headers=self.client_session.last_response_headers, data=result)
 
-    def create_item(self, body: "Dict[str,str]") -> "Item":
-        result = self._context.CreateItem(
+    def create_item(self, body: "Dict[str, Any]") -> "Item":
+        result = self.client_session.CreateItem(
             database_or_Container_link=self.collection_link, document=body
         )
-        return Item(self, headers=self._context.last_response_headers, data=result)
+        return Item(headers=self.client_session.last_response_headers, data=result)
 
     def delete_item(self, item: "Item"):
         document_link = Container._document_link(item)
-        self._context.DeleteItem(document_link=document_link)
+        self.client_session.DeleteItem(document_link=document_link)
 
---
+++ prototype/examples.py
@@ -0,0 +1,68 @@
+from azure.cosmos.errors import HTTPFailure
+
+from client import Client as CosmosClient, Container
+
+# It all starts with a client instance:
+import os
+url = os.environ['ACCOUNT_HOST']
+key = os.environ['ACCOUNT_KEY']
+client = CosmosClient(url, key)
+
+test_database_name = 'testDatabase'
+test_container_name = 'testContainer'
+#### Create environnment ###
+db = client.create_database(id=test_database_name, fail_if_exists=False)
+try:
+    db.create_container(id=test_container_name)
+except HTTPFailure as e:
+    if e.status_code != 409:
+        raise
+    db.get_container(test_container_name)
+
+####
+
+# In order to retreive a container where you know the database name and container name:
+container = Container(client.client_session, database=test_database_name, id=test_container_name)
+container.upsert_item({
+    'id': 'something',
+    'value': 'else'
+})
+
+# If you want to walk down the hierarchy, you can also get it from the client->databases->containers
+database = client.get_database(test_database_name)
+container = database.get_container(test_container_name)
+container.upsert_item({
+    'id': 'something',
+    'value': 'new'
+})
+
+# Once you have a container, you can query items to your heart's content:
+items = list(container.query_items(query='SELECT * FROM root r WHERE r.id="something"'))
+
+# You can enumerate the items:
+import json
+for item in items:
+    print(json.dumps(item, indent=True))
+
+# It should be (almost) free to ask the length of a query
+# if len(items) > 4711: # TODO: NYI (should use header to get this info to make it O(1))
+#    print('Big number')
+
+# If you want to create things, you can just go ahead and create some dicts
+for i in range(1, 10):
+    container.upsert_item(dict(
+        id=f'item{i}',
+        firstName='David',
+        lastName='Smith'
+        )
+    )
+
+# You can also modify one of the existing items
+item = items[0]
+item['firstName'] = 'Some Other Name'
+updated_item = container.upsert_item(item) # ISSUE: do we update the item "in place" for system properties and  headers?
+
+# If you need to get the properties of a database, they are all there...
+properties = database.properties
+print(json.dumps(properties))
+
