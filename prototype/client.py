d955b36551122e8258f8b87e97ee627728457d6f
Johan Stenberg <johan.stenberg@microsoft.com>
2018-11-10 18:16:26 -0800

    Move mixins to the database/collection/client classes (improves docs)

--- prototype/client.py
+++ prototype/client.py
@@ -1,7 +1,7 @@
 """
 Cosmos client Pythonidae
 """
-from typing import Any, Iterable, Optional, Dict, Union, Tuple, cast
+from typing import Any, Iterable, Optional, Dict, Union, Tuple, cast, overload
 
 
 from azure.cosmos.cosmos_client import CosmosClient as _CosmosClient
@@ -77,173 +77,7 @@ class UserDefinedFunctionsMixin:
         pass
 
 
-class ContainersManagementMixin:
-    """
-    Manage (create/list/query/get/delete) containers. 
-    [Design note] Currently isolated into a mixin class to make it easier to move the functionality around
-    in the object model. 
-    """
-
-    def create_container(
-        self, id, options=None, **kwargs
-    ) -> "Container":
-        """
-        :param str id: Id of container to create
-
-        Keyword arguments:
-        partitionKey, indexingPolicy, defaultTtl, conflictResolutionPolicy
-        """
-        definition = dict(id=id)
-        definition.update(kwargs)
-        database = cast("Database", self)
-        data = database.client_session.CreateContainer(
-            database_link=database.database_link,
-            collection=definition,
-            options=options,
-        )
-        return Container(
-            cast("Database", self).client_session, database, data["id"]
-        )
-
-    def delete_container(self, container_or_id):
-        database = cast("Database", self)
-        if isinstance(container_or_id, str):
-            container = Container(database, container_or_id)
-        else:
-            container = container_or_id
-        database._context.DeleteContainer(container.collection_link)
-
-    def get_container(self, id):
-        containers = self.list_containers(
-            query=dict(
-                query="SELECT * FROM root r WHERE r.id = @container",
-                parameters=[{"name": "@container", "value": id}],
-            )
-        )
-        try:
-            return next(containers)
-        except StopIteration:
-            raise ValueError()
-
-    def list_containers(self, query=None) -> "Iterable[Container]":
-        database = cast("Database", self)
-
-        if query:
-            yield from [
-                Container(database.client_session, database, container["id"])
-                for container in database.client_session.ReadContainers(
-                    database_link=database.database_link
-                )
-            ]
-        else:
-            yield from [
-                Container(database.client_session, database, container["id"])
-                for container in database.client_session.ReadContainers(
-                    database_link=database.database_link
-                )
-            ]
-
-    def set_container_properties(
-        self,
-        container: "Union[str, Container]",
-        *,
-        partition_key=None,
-        indexing_policy=None,
-        default_ttl=None,
-        conflict_resolution_policy=None,
-    ):
-        """
-        Update the properties of the container. Change will be persisted immediately.
-        """
-        id = getattr(container, "id", container)
-        parameters = {
-            key: value
-            for key, value in {
-                "id": id,
-                "partitionKey": partition_key,
-                "indexingPolicy": indexing_policy,
-                "defaultTtl": int(default_ttl),
-                "conflictResolutionPolicy": conflict_resolution_policy,
-            }.items()
-            if value
-            is not None  # TODO: Questionable use - should use kwargs instead. Need to figure out best documentation for kwargs...
-        }
-        database = cast("Database", self)
-        collection_link = f"{database.database_link}/colls/{id}"
-        database.client_session.ReplaceContainer(collection_link, collection=parameters)
-
-    def get_container_properties(self, container):
-        """
-        Get properties for of container. TODO: implement properly
-        """
-        pass
-
-
-class ItemsManagementMixin:
-    pass
-
-
-class DatabasesManagementMixin:
-    def create_database(self, id: "str", fail_if_exists=False) -> "Database":
-        """
-        Create a new database
-        :param str id: Id of the database to crate.
-        :param bool fail_if_exists: If set to True and a database with the given `id` 
-        already exists, fail raise a `HTTPFailure`/status code 409. If a database with
-        the given id exists, and `fail_if_exists` is False, return the existing database.
-        """
-        try:
-            client_session = cast("Client", self).client_session
-            result = client_session.CreateDatabase(database=dict(id=id))
-            return Database(client_session, id=result["id"], properties=result)
-        except HTTPFailure as e:
-            if fail_if_exists and e.status_code == 409:
-                raise
-        return self.get_database(id)
-
-    def get_database(self, id: "str") -> "Database":
-        """
-        Return the existing database with the id `id. 
-        :param str id: Id of the new database.
-        """
-        client_session = cast("Client", self).client_session
-        properties = client_session.ReadDatabase(database_link=f"dbs/{id}")
-        database = Database(client_session, id, properties)
-        return database
-
-    def get_database_properties(self, id: "str"):
-        client_session = cast("Client", self).client_session
-        properties = client_session.ReadDatabase(id)
-        return properties
-
-    def list_databases(self, query: "Optional[str]" = None) -> "Iterable[Database]":
-        """
-        Return an iterable of all existing databases.
-        :param str query: Cosmos DB SQL query. If omitted, all databases will be listed.
-        """
-        client_session = cast("Client", self).client_session
-        if query:
-            yield from [
-                Database(client_session, properties["id"], properties)
-                for properties in client_session.QueryDatabases(query)
-            ]
-        else:
-            yield from [
-                Database(client_session, properties["id"], properties)
-                for properties in client_session.ReadDatabases()
-            ]
-
-    def delete_database(self, id: "str"):
-        """
-        Delete the database with the given id. Raises a HTTPError if 
-        the delete fails. 
-        :param str id: The database to delete. 
-        """
-        client_session = cast("Client", self).client_session
-        client_session.DeleteDatabase(database_link=f"dbs/{id}")
-
-
-class Client(DatabasesManagementMixin):
+class Client:
     """
     CosmosDB SQL Client. This is the main entry point to the Cosmos DB object model.
 
@@ -257,8 +91,78 @@ class Client(DatabasesManagementMixin):
     def __init__(self, url, key):
         self.client_session = ClientSession(url, dict(masterKey=key))
 
+    def create_database(self, id: "str", fail_if_exists=False) -> "Database":
+        """ Create a new database with the given name (id)
 
-class Database(ContainersManagementMixin, UsersManagementMixin):
+        :param id: Id (name) of the database to create.
+        :param bool fail_if_exists: Fail if database already exists.
+        :raises azure.cosmos.errors.HTTPFailure: If `fail_if_exists` is set to True and a database with the given id already exists
+
+        :Example: Creating a new database
+
+        >>> import os
+        >>> ACCOUNT_KEY = os.environ['ACCOUNT_KEY']
+        >>> ACCOUNT_HOST = os.environ['ACCOUNT_HOST']
+        >>> client = Client(url=ACCOUNT_HOST, key=ACCOUNT_KEY)
+        >>> database = client.create_database('nameofdatabase')        
+        ...
+
+        """
+        try:
+            result = self.client_session.CreateDatabase(database=dict(id=id))
+            return Database(self.client_session, id=result["id"], properties=result)
+        except HTTPFailure as e:
+            if fail_if_exists and e.status_code == 409:
+                raise
+        return self.get_database(id)
+
+    def get_database(self, id: "str") -> "Database":
+        """
+        Retreive the existing database with the id (name) `id`. 
+
+        :param id: Id of the new database.
+        :raise HTTPError: If the given database couldn't be retrieved.
+        """
+        properties = self.client_session.ReadDatabase(database_link=f"dbs/{id}")
+        database = Database(self.client_session, id, properties)
+        return database
+
+    def get_database_properties(self, id: "str"):
+        """ Get the database properties 
+
+        :param id: Id (or name) of the database to retrieve properties for.
+        """
+        properties = self.client_session.ReadDatabase(id)
+        return properties
+
+    def list_databases(self, query: "Optional[str]" = None) -> "Iterable[Database]":
+        """
+        Return all databases matching the query, or all databases if query is not provided
+
+        :param query: Cosmos DB SQL query. If omitted, all databases will be listed.
+        """
+        if query:
+            yield from [
+                Database(self.client_session, properties["id"], properties)
+                for properties in self.client_session.QueryDatabases(query)
+            ]
+        else:
+            yield from [
+                Database(self.client_session, properties["id"], properties)
+                for properties in self.client_session.ReadDatabases()
+            ]
+
+    def delete_database(self, id: "str"):
+        """
+        Delete the database with the given id.
+
+        :param id: The id (name) of the database to delete. 
+        :raise azure.cosmos.HTTPError: If the call to delete the database fails.
+        """
+        self.client_session.DeleteDatabase(database_link=f"dbs/{id}")
+
+
+class Database(UsersManagementMixin):
     """
     Azure Cosmos DB SQL Database
     """
@@ -274,6 +178,108 @@ class Database(ContainersManagementMixin, UsersManagementMixin):
         self.id = id
         self.properties = properties
         self.database_link = f"/dbs/{self.id}"
+
+    def create_container(self, id, options=None, **kwargs) -> "Container":
+        """
+        Create a new container with the given id (name). If a container with the given
+        id (name) already exists, an HTTPError will be raised.
+
+        :param str id: Id of container to create
+        :raise HTTPError:
+
+        Keyword arguments:
+        partitionKey, indexingPolicy, defaultTtl, conflictResolutionPolicy
+        """
+        definition = dict(id=id)
+        definition.update(kwargs)
+        data = self.client_session.CreateContainer(
+            database_link=self.database_link, collection=definition, options=options
+        )
+        return Container(self.client_session, self, data["id"])
+
+    @overload
+    def delete_container(self, container: "str"):
+        ...
+
+    @overload
+    def delete_container(self, container: "Container"):
+        ...
+
+    def delete_container(self, container):
+        """ Delete the container
+
+        :param container: The container to delete. 
+        """
+        collection_link = getattr(
+            container, "collection_link", f"{self.database_link}/col/{container}"
+        )
+        properties = self.client_session.DeleteContainer(collection_link)
+
+    def get_container(self, id):
+        containers = self.list_containers(
+            query=dict(
+                query="SELECT * FROM root r WHERE r.id = @container",
+                parameters=[{"name": "@container", "value": id}],
+            )
+        )
+        try:
+            return next(containers)
+        except StopIteration:
+            raise ValueError()
+
+    def list_containers(self, query=None) -> "Iterable[Container]":
+        if query:
+            yield from [
+                Container(self.client_session, self, container["id"])
+                for container in self.client_session.ReadContainers(
+                    database_link=self.database_link
+                )
+            ]
+        else:
+            yield from [
+                Container(self.client_session, self, container["id"])
+                for container in self.client_session.ReadContainers(
+                    database_link=self.database_link
+                )
+            ]
+
+    def set_container_properties(
+        self,
+        container: "Union[str, Container]",
+        *,
+        partition_key=None,
+        indexing_policy=None,
+        default_ttl=None,
+        conflict_resolution_policy=None,
+    ):
+        """
+        Update the properties of the container. Change will be persisted immediately.
+        """
+        container_id = getattr(container, "id", container)
+        parameters = {
+            key: value
+            for key, value in {
+                "id": container_id,
+                "partitionKey": partition_key,
+                "indexingPolicy": indexing_policy,
+                "defaultTtl": int(default_ttl),
+                "conflictResolutionPolicy": conflict_resolution_policy,
+            }.items()
+            if value
+            is not None  # TODO: Questionable use - should use kwargs instead. Need to figure out best documentation for kwargs...
+        }
+        collection_link = f"{self.database_link}/colls/{container_id}"
+        self.client_session.ReplaceContainer(collection_link, collection=parameters)
+
+    def get_container_properties(self, container) -> "Dict[str, Any]":
+        """
+        Get properties for of container. TODO: implement properly
+        """
+        collection_link = getattr(
+            container, "collection_link", f"{self.database_link}/cols/{container}"
+        )
+        properties = self.client_session.ReadContainer(collection_link)
+        return properties
 
 
 class Item(dict):
@@ -293,7 +299,7 @@ class Container(UserDefinedFunctionsMixin, TriggersMixin, StoredProceduresMixin)
         self.client_session = client_session
         self.session_token = None
         self.id = id
-        database_link = getattr(database, "database_link", f'dbs/{database}')
+        database_link = getattr(database, "database_link", f"dbs/{database}")
         self.collection_link = f"{database_link}/colls/{self.id}"
 
     @staticmethod
