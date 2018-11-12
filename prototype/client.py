4e4ba2ff66548db1c587c37fdc09973c182d2399
Johan Stenberg <johan.stenberg@microsoft.com>
2018-11-12 11:14:15 -0800

    Re-rename ClientSession to ClientContext. Introduced response classes.

--- prototype/client.py
+++ prototype/client.py
@@ -8,23 +8,11 @@ from azure.cosmos.cosmos_client import CosmosClient as _CosmosClient
 from azure.cosmos.errors import HTTPFailure
 
 
-class ClientSession(_CosmosClient):
+class ClientContext(_CosmosClient):
     pass
 
-
-class UsersManagementMixin:
-    def create_user(self, user):
-        pass
-
-    def get_user(self, id):
-        pass
-
-    def list_users(self):
-        pass
-
-    def delete_user(self):
-        pass
-
+class User:
+    pass
 
 class StoredProceduresMixin:
     def list_stored_procedures(self, query):
@@ -89,7 +77,7 @@ class Client:
     """
 
     def __init__(self, url, key):
-        self.client_session = ClientSession(url, dict(masterKey=key))
+        self.client_context = ClientContext(url, dict(masterKey=key))
 
     def create_database(self, id: "str", fail_if_exists=False) -> "Database":
         """ Create a new database with the given name (id)
@@ -109,8 +97,10 @@ class Client:
 
         """
         try:
-            result = self.client_session.CreateDatabase(database=dict(id=id))
-            return Database(self.client_session, id=result["id"], properties=result)
+            result = self.client_context.CreateDatabase(database=dict(id=id))
+            return DatabaseResponse(
+                self.client_context, id=result["id"], properties=result
+            )
         except HTTPFailure as e:
             if fail_if_exists and e.status_code == 409:
                 raise
@@ -123,8 +113,8 @@ class Client:
         :param id: Id of the new database.
         :raise HTTPError: If the given database couldn't be retrieved.
         """
-        properties = self.client_session.ReadDatabase(database_link=f"dbs/{id}")
-        database = Database(self.client_session, id, properties)
+        properties = self.client_context.ReadDatabase(database_link=f"dbs/{id}")
+        database = DatabaseResponse(self.client_context, id, properties)
         return database
 
     def get_database_properties(self, id: "str"):
@@ -132,7 +122,7 @@ class Client:
 
         :param id: Id (or name) of the database to retrieve properties for.
         """
-        properties = self.client_session.ReadDatabase(id)
+        properties = self.client_context.ReadDatabase(id)
         return properties
 
     def list_databases(self, query: "Optional[str]" = None) -> "Iterable[Database]":
@@ -143,13 +133,13 @@ class Client:
         """
         if query:
             yield from [
-                Database(self.client_session, properties["id"], properties)
-                for properties in self.client_session.QueryDatabases(query)
+                DatabaseResponse(self.client_context, properties["id"], properties)
+                for properties in self.client_context.QueryDatabases(query)
             ]
         else:
             yield from [
-                Database(self.client_session, properties["id"], properties)
-                for properties in self.client_session.ReadDatabases()
+                DatabaseResponse(self.client_context, properties["id"], properties)
+                for properties in self.client_context.ReadDatabases()
             ]
 
     def delete_database(self, id: "str"):
@@ -159,27 +149,24 @@ class Client:
         :param id: The id (name) of the database to delete. 
         :raise azure.cosmos.HTTPError: If the call to delete the database fails.
         """
-        self.client_session.DeleteDatabase(database_link=f"dbs/{id}")
+        self.client_context.DeleteDatabase(database_link=f"dbs/{id}")
 
 
-class Database(UsersManagementMixin):
+class Database:
     """
     Azure Cosmos DB SQL Database
     """
 
-    def __init__(
-        self, client_session: "ClientSession", id: "str", properties: "Dict[str, Any]"
-    ):
+    def __init__(self, client_context: "ClientContext", id: "str"):
         """
-        :param ClientSession client_session: Client from which this database was retreived.
+        :param ClientSession client_context: Client from which this database was retreived.
         :param str id: Id of the database
         """
-        self.client_session = client_session
+        self.client_context = client_context
         self.id = id
-        self.properties = properties
         self.database_link = f"/dbs/{self.id}"
 
-    def create_container(self, id, options=None, **kwargs) -> "Container":
+    def create_container(self, id, options=None, **kwargs) -> "ContainerResponse":
         """
         Create a new container with the given id (name). If a container with the given
         id (name) already exists, an HTTPError will be raised.
@@ -192,10 +179,10 @@ class Database(UsersManagementMixin):
         """
         definition = dict(id=id)
         definition.update(kwargs)
-        data = self.client_session.CreateContainer(
+        data = self.client_context.CreateContainer(
             database_link=self.database_link, collection=definition, options=options
         )
-        return Container(self.client_session, self, data["id"])
+        return ContainerResponse(self.client_context, self, data["id"], properties=data)
 
     @overload
     def delete_container(self, container: "str"):
@@ -211,34 +198,35 @@ class Database(UsersManagementMixin):
         :param container: The container to delete. 
         """
         collection_link = getattr(
-            container, "collection_link", f"{self.database_link}/col/{container}"
+            container, "collection_link", f"{self.database_link}/colls/{container}"
         )
-        properties = self.client_session.DeleteContainer(collection_link)
+        properties = self.client_context.DeleteContainer(collection_link)
 
-    def get_container(self, id):
-        containers = self.list_containers(
-            query=dict(
-                query="SELECT * FROM root r WHERE r.id = @container",
-                parameters=[{"name": "@container", "value": id}],
-            )
+    def get_container(
+        self, container_or_id: "Union[str, Container]"
+    ) -> "ContainerResponse":
+        collection_link = getattr(
+            container_or_id,
+            "collection_link",
+            f"{self.database_link}/colls/{container_or_id}",
         )
-        try:
-            return next(containers)
-        except StopIteration:
-            raise ValueError()
+        container = self.client_context.ReadContainer(collection_link)
+        return ContainerResponse(
+            self.client_context, self, container["id"], properties=container
+        )
 
-    def list_containers(self, query=None) -> "Iterable[Container]":
+    def list_containers(self, query=None) -> "Iterable[ContainerResponse]":
         if query:
             yield from [
-                Container(self.client_session, self, container["id"])
-                for container in self.client_session.ReadContainers(
+                ContainerResponse(self.client_context, self, container["id"], container)
+                for container in self.client_context.ReadContainers(
                     database_link=self.database_link
                 )
             ]
         else:
             yield from [
-                Container(self.client_session, self, container["id"])
-                for container in self.client_session.ReadContainers(
+                ContainerResponse(self.client_context, self, container["id"], container)
+                for container in self.client_context.ReadContainers(
                     database_link=self.database_link
                 )
             ]
@@ -269,7 +257,7 @@ class Database(UsersManagementMixin):
             is not None  # TODO: Questionable use - should use kwargs instead. Need to figure out best documentation for kwargs...
         }
         collection_link = f"{self.database_link}/colls/{container_id}"
-        self.client_session.ReplaceContainer(collection_link, collection=parameters)
+        self.client_context.ReplaceContainer(collection_link, collection=parameters)
 
     def get_container_properties(self, container) -> "Dict[str, Any]":
         """
@@ -278,8 +266,39 @@ class Database(UsersManagementMixin):
         collection_link = getattr(
             container, "collection_link", f"{self.database_link}/cols/{container}"
         )
-        properties = self.client_session.ReadContainer(collection_link)
+        properties = self.client_context.ReadContainer(collection_link)
         return properties
+
+    def get_user_link(self, id_or_user: "Union[User, str]") -> "str":
+        user_link = getattr(id_or_user, 'user_link', f'{self.database_link}/users/{id_or_user}')
+        return user_link
+
+    def create_user(self, user, options=None):
+        database = cast("Database", self)
+        return database.client_context.CreateUser(database.database_link, user, options)
+
+    def get_user(self, id):
+        database = cast("Database", self)
+        return database.client_context.ReadUser(self.get_user_link(id))
+
+    def list_users(self, query=None):
+        database = cast("Database", self)
+        yield from [
+            User(user)
+            for user in database.client_context.ReadUsers(query)
+        ]
+
+    def delete_user(self, user):
+        database = cast("Database", self)
+        database.client_context.DeleteUser(self.get_user_link(id))
+
+
+class DatabaseResponse(Database):
+    def __init__(
+        self, client_context: "ClientContext", id: "str", properties: "Dict[str, Any]"
+    ):
+        super().__init__(client_context, id)
+        self.properties = properties
 
 
 class Item(dict):
@@ -292,11 +311,11 @@ class Item(dict):
 class Container(UserDefinedFunctionsMixin, TriggersMixin, StoredProceduresMixin):
     def __init__(
         self,
-        client_session: "ClientSession",
+        client_context: "ClientContext",
         database: "Union[Database, str]",
         id: "str",
     ):
-        self.client_session = client_session
+        self.client_context = client_context
         self.session_token = None
         self.id = id
         database_link = getattr(database, "database_link", f"dbs/{database}")
@@ -317,51 +336,62 @@ class Container(UserDefinedFunctionsMixin, TriggersMixin, StoredProceduresMixin)
         :returns: Item if present.
         """
         doc_link = f"{self.collection_link}/docs/{id}"
-        result = self.client_session.ReadItem(document_link=doc_link)
-        headers = self.client_session.last_response_headers
+        result = self.client_context.ReadItem(document_link=doc_link)
+        headers = self.client_context.last_response_headers
         self.session_token = headers.get("x-ms-session-token", self.session_token)
         return Item(headers=headers, data=result)
 
     def list_items(self, options=None) -> "Iterable[Item]":
         options = options or {}
-        items = self.client_session.ReadItems(
+        items = self.client_context.ReadItems(
             collection_link=self.collection_link, feed_options=options
         )
-        headers = self.client_session.last_response_headers
+        headers = self.client_context.last_response_headers
         yield from [Item(headers=headers, data=item) for item in items]
 
     def query_items(
         self, query: "str", options=None, partition_key: "Optional[str]" = None
-    ):
-        items = self.client_session.QueryItems(
+    ) -> "Iterable[Item]":
+        items = self.client_context.QueryItems(
             database_or_Container_link=self.collection_link,
             query=query,
             options=options,
             partition_key=partition_key,
         )
-        headers = self.client_session.last_response_headers
+        headers = self.client_context.last_response_headers
         yield from [Item(headers, item) for item in items]
 
     def replace_item(self, item: "Union[Item, str]", body: "Dict[str, Any]") -> "Item":
         item_link = Container._document_link(item)
-        data = self.client_session.ReplaceItem(
+        data = self.client_context.ReplaceItem(
             document_link=item_link, new_document=body
         )
-        return Item(headers=self.client_session.last_response_headers, data=data)
+        return Item(headers=self.client_context.last_response_headers, data=data)
 
     def upsert_item(self, body: "Dict[str, Any]") -> "Item":
-        result = self.client_session.UpsertItem(
+        result = self.client_context.UpsertItem(
             database_or_Container_link=self.collection_link, document=body
         )
-        return Item(headers=self.client_session.last_response_headers, data=result)
+        return Item(headers=self.client_context.last_response_headers, data=result)
 
     def create_item(self, body: "Dict[str, Any]") -> "Item":
-        result = self.client_session.CreateItem(
+        result = self.client_context.CreateItem(
             database_or_Container_link=self.collection_link, document=body
         )
-        return Item(headers=self.client_session.last_response_headers, data=result)
+        return Item(headers=self.client_context.last_response_headers, data=result)
 
-    def delete_item(self, item: "Item"):
+    def delete_item(self, item: "Item") -> "None":
         document_link = Container._document_link(item)
-        self.client_session.DeleteItem(document_link=document_link)
+        self.client_context.DeleteItem(document_link=document_link)
 
+
+class ContainerResponse(Container):
+    def __init__(
+        self,
+        client_context: "ClientContext",
+        database: "Union[Database, str]",
+        id: "str",
+        properties: "Dict[str, Any]",
+    ):
+        super().__init__(client_context, database, id)
+        self.properties = properties
